80cce76695a298b06173f4f5807bc37b69394bc4
LordAthetos <75067362+LordAthetos@users.noreply.github.com>
2022-03-04 18:54:16 +0100

    comentario

--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Slider : MonoBehaviour
 {
+    // cambia la posicion en Y segun el slider
     public void ChangePosY(float yPos)
     {
         transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
